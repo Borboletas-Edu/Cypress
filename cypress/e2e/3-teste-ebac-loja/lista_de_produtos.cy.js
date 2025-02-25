ba2e942cad34a7058ae6ed974c28b4cdd17e7594
Edu <eduardo.a.soares2005@gmail.com>
2025-02-25 20:08:40 -0300

    Testes com listas de itens no cypress

---
+++ cypress/e2e/3-teste-ebac-loja/lista_de_produtos.cy.js
@@ -0,0 +1,28 @@
+/// <reference types='cypress'/>
+
+describe('Análise na lista de produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/') 
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Entrada na página de descrição detalhada do primeiro produto', () => {
+        cy.get('.image ').first().click()
+        cy.get('#tab-title-description').should('contain', 'Descrição')
+    });
+       
+    it('Entrada na página de descrição detalhada do ultimo produto', () => {
+        cy.get('.image ').last().click()
+        cy.get('#tab-title-description').should('contain', 'Descrição')
+    });
+
+    it('Entrada na página de descrição detalhada do quinto produto', () => {
+        cy.get('.image ').eq(5).click()
+        cy.get('#tab-title-description').should('contain', 'Descrição')
+    });
+
+});
